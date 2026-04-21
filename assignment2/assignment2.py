2ab7d05b971f2f353e9b900a83dd8c510448c67d
Sumna Chea <sumnachea85@gmail.com>
2026-04-21 10:35:50 -0400

    revisions to assignment2

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,5 +1,6 @@
 #task 2:
 import csv
+import traceback
 
 def read_employees():
     employees_dict = dict()
@@ -17,7 +18,15 @@ def read_employees():
             return employees_dict
         
     except Exception as e:
-        print(e)
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = list()
+        for trace in trace_back:
+            stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
+        if message:
+            print(f"Exception message: {message}")
+        print(f"Stack trace: {stack_trace}")
         
 employees = read_employees()
 print(employees)
@@ -117,7 +126,7 @@ import custom_module
 def set_that_secret(new_secret):
     custom_module.set_secret(new_secret)
 
-set_that_secret("hello")
+set_that_secret("swordfish")
 print("Secret word:", custom_module.secret)
 
 #task 12:
